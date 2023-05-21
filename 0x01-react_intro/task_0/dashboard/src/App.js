39d3073719e6ad7160119748be18c3bf06334a74
Bennet-Ukoh <bennetukoh@gmail.com>
2023-05-21 06:19:59 +0100

    task 5

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import HolbertonLogo from './HolbertonLogo.jpg';
+import { getFooterCopy, getFullYear } from './utils'
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={HolbertonLogo} alt='HolbertonLogo' className='logo'/>
+        <img src={ HolbertonLogo } alt='HolbertonLogo' className='logo'/>
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email:</label>
+        <input type='text' id='email' />
+
+        <label htmlFor='password'>Password:</label>
+        <input type='password' id='password' /> 
+      
+        <button>Ok</button>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>Copyright { getFullYear() } - { getFooterCopy(false) }</p>
       </div>
     </div>
   );
