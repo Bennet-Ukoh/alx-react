5f6e03c942f93ee6a4d2c977274ea28acd3b2797
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-19 08:17:22 +0000

    updated 0-fromjs

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,8 +1,3 @@
-// Import the Immutable.js library
-const { fromJS } = require('immutable');
-
-// Define the getImmutableObject function
 export const getImmutableObject = (object) => {
-  // Convert the JavaScript object to an Immutable Map
   return fromJS(object);
 };
