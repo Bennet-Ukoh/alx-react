8d99c9f1721862ab4d72d7dc586dcbd0529a7d16
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-19 09:48:43 +0000

    6-deeply.js

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,16 @@
+import { Map, List, fromJS } from 'immutable';
+
+// Function to merge deeply nested objects
+export function mergeDeeplyElements(page1, page2) {
+  // Convert the objects to Immutable Maps
+  const map1 = fromJS(page1);
+  const map2 = fromJS(page2);
+
+  // Merge the two Maps deeply, with page2 values taking precedence and combining duplicate values
+  const mergedMap = map1.mergeDeep(map2);
+
+  // Convert the merged Map values to an Immutable List
+  const mergedList = mergedMap.toList();
+
+  return mergedList;
+}
