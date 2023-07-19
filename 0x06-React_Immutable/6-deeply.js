fd9ba6936fbe10c1b855970bc5573865df15381b
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-19 10:09:21 +0000

    lint fixed

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,12 +1,10 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
 // Function to merge deeply nested objects
 export function mergeDeeplyElements(page1, page2) {
   // Convert the objects to Immutable Maps
   const map1 = Map(page1);
   const map2 = Map(page2);
-
-  // Merge the two Maps deeply, with page2 values taking precedence and combining duplicate values
   const mergedMap = map1.mergeDeep(map2);
 
   // Convert the merged Map values to an Immutable List
