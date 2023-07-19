60c45008ea370ae44589483801f446961d9a80de
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-19 08:32:07 +0000

    error fixed

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,3 +1,12 @@
-export const getImmutableObject = (object) => {
-  return fromJS(object);
+// Import the 'fromJS' function from Immutable.js
+import { fromJS } from 'immutable';
+
+// Define the function 'getImmutableObject'
+const getImmutableObject = (object) => {
+  // Convert the input object into an immutable Map using 'fromJS'
+  const immutableMap = fromJS(object);
+  return immutableMap;
 };
+
+// Export the function to make it accessible from other files
+export default getImmutableObject;
